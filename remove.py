be9e31ce55271b9f561af0c6d13517ad05db561b
Oning20 <jingleestrada16@gmail.com>
2024-10-07 09:43:12 +0800

    my first

--- remove.py
+++ remove.py
@@ -1,12 +1,12 @@
-numbers = ("gwapo", "pogi", "handsome")
+name = ["gwapo", "pogi", "handsome"]
+print(name)
 
-kuan = input("Do you want to delete it by index or by name?" " " "if by index type INDEX " " " "if by name type NAME:")
-if kuan == "INDEX":
-    names = int(input("type a num:"))
-    numbers.pop(names)
- 
-elif kuan == "NAME":
-    names = input("type a name you want to remove:")
-    names.remove(numbers)
+kuan = input("Do you want to delete it by index or by name?" " " "if by index type index " " " "if by name type names :")
 
-print(numbers)
+if kuan == "index":
+    name.pop(int(input("select num :")))
+    print(name)
+
+elif kuan == "names":
+ name.remove(input("select name :"))
+ print(name)
